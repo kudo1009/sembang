fffda492024aea54427b12903deea7fe3bb2d1fd
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-08-14 13:47:49 +0000

    First commit

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module Berborak
+  class Application < Rails::Application
+  
+  config.load_defaults 5.1
+  config.i18n.default_locale = :ja
+
+  end
+end
