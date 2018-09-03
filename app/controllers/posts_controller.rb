a64b112d313c2299a584c74be5a8ebd2bda347c2
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-09-03 14:28:48 +0000

    init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   
   def show
     @post = Post.find_by(id: params[:id])
+    @favorite = current_user.favorites.find_by(post_id: @post.id)
     @user = @post.user
   end
   
