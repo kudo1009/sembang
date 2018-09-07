634c71b491ec6ab2d0913ca4281415829722624d
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-09-07 16:56:35 +0000

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://quiet-tundra-70071.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: ENV['app108032667@heroku.com'],
+    password: ENV['23qx8mre5806'],
     domain: "heroku.com",
     address: "smtp.SendGrid.net",
     port: 587,
