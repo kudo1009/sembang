6378089dd4af1043c9ed15b75f902439f51ca181
Kohei Kudo <koheikudo1009@yahoo.co.jp>
2018-09-16 05:05:00 +0000

    init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180910060455) do
+ActiveRecord::Schema.define(version: 20180911104720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180910060455) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.string "image_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
